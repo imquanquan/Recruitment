515507dadddf34f247fa6945c88f6e54409b6a00
lijinquan <lijinquan@corp.netease.com>
2021-01-11 20:38:25 +0800

    完成 jobhunter urls 设计

---
+++ recruit/jobhunter.py
@@ -0,0 +1,31 @@
+"""
+这里写 jobhunter（求职者）相关业务处理逻辑
+"""
+
+
+def register(request):
+    """
+    求职者注册
+    :param request:
+    :return:
+    """
+    pass
+
+
+def login(request):
+    """
+    求职者登陆
+    :param request:
+    :return:
+    """
+    pass
+
+
+def info(request, jobhunter_id):
+    """
+    求职者详细信息
+    :param request:
+    :param jobhunter_id:
+    :return:
+    """
+    pass
